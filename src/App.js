a2688105d1e6b40f416ddd35d75c6c928a9a5ee7
Fikri Rida Pebriansyah <fikriultimate18@gmail.com>
2021-09-13 21:04:45 +0700

    Adding imagelist route

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Login from "./pages/Login";
+import Surveillance from "./pages/Surveillance";
+import Imagelist from "./pages/Imagelist";
 import Dashboard from "./pages/Dashboard";
 import Document from "./pages/Document";
 import Dataset from "./pages/Dataset";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route path="/login" children={<Login />} />
+        <Route
+          path="/surveillance/:dataset_id/:device_id"
+          children={<Surveillance />}
+        />
+        <Route path="/imagelist/:dataset_id" children={<Imagelist />} />
         <Route path="/dataset/:dataset_id/:device_id" children={<Document />} />
         <Route path="/dataset/:dataset_id" children={<Dataset />} />
         <Route path="/" children={<Dashboard />} />
